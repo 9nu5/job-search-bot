d02eec371de6b1375c568b8ba336acfb9efdde5b
9nu5 <hhh990679@gmail.com>
2026-02-12 17:48:12 +0900

    feat: implement main orchestrator for job data pipeline

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-import pandas as pd
-import requests
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+from src.storage.minio_storage import MinioStorage
 
-test="test"
+def main():
+    load_dotenv()
+    
+    endpoint= f"localhost:{os.getenv('MINIO_PORT')}"
+    access_key = os.getenv('MINIO_ROOT_USER')
+    secret_key = os.getenv('MINIO_ROOT_PASSWORD')
+    bucket_name = os.getenv('MINIO_BUCKET_NAME')
+    
+    today=datetime.now().strftime("%Y_%m_%d")
+    
+    json_path= "data/scout_green_jobs.json"
+    parquet_path=f"data/{today}_green_jobs.parquet"
+    file_path=parquet_path
+    object_name =f"data/raw_data/{today}_jobs.parquet"
+    
+    minio=MinioStorage(endpoint,access_key,secret_key)
+    
+    minio.save_json_as_parquet(json_path,parquet_path)
+    minio.upload_file(bucket_name,object_name,file_path)
 
-print(test)
-print("pandas :", pd.__version__)
+    print("むこ!!!")
+
+if __name__ == "__main__":
+    main()
